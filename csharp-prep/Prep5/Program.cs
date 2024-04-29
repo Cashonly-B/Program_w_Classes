2cb7cd16c5f993c1037182b00482a10749fa5540
Cashonly-B <bon23011@byui.edu>
2024-04-29 16:26:29 -0600

    FInished Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,6 +6,54 @@ class Program
     {
         Console.WriteLine("Hello Prep5 World!");
 
-        
+        // Display Welcome
+        static void DisplayWelcome()
+        {
+	        Console.WriteLine("Welcome to the program!");
+        }
+
+        // Gets user's name
+        static string GetsUsersName()
+        {
+        	Console.Write("Please Enter Your Name: ");
+            string usersName = Console.ReadLine();
+            return usersName;
+        } 
+
+        // Gets favorite number
+        static int GetsUsersNumber()
+        {
+            Console.Write("Please Enter Your Favorite Number: ");
+            string usersNumber = Console.ReadLine();
+            int favNumber = int.Parse(usersNumber);
+            return favNumber;
+        }
+
+        // Squares the number
+        static int SquareNumber(int favNumber)
+        {
+            // Squaring for some reason wasn't working for me, so I just did this
+            int total = favNumber * favNumber;
+            return total;
+        }
+
+        // Displays the results at the end
+        static void DisplayResults(string usersName, int total)
+        {
+            Console.WriteLine($"{usersName}, the square of your number is {total}");
+        }
+
+        // Main
+        static void main()
+        {
+            DisplayWelcome();
+            string usersName = GetsUsersName();
+            int usersNumber = GetsUsersNumber();
+            int squaredNumber = SquareNumber(usersNumber);
+            DisplayResults(usersName, squaredNumber);
+        }
+
+        main();
+
     }
 }
