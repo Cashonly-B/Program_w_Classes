267d68bad8f4a3a11abc5f331ff28f0fe4994d3d
Cashonly-B <bon23011@byui.edu>
2024-05-20 13:26:57 -0600

    Did Learning3 Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+
+        Person p = new Person();
+        p.SetFirstName("John");
+        p.SetLastName("Smith");
+        p.SetTitle("Walmart");
+        Console.WriteLine($"\nHello {p.GetFirstName()} {p.GetLastName()} from {p.GetTitle()}!\n");
+
     }
+}
+public class Person 
+{
+    private string _firstName;
+    private string _lastName;
+    private string _title;
+
+    public string GetFirstName() { return _firstName; }
+    public string GetLastName() { return _lastName; }
+    public string GetTitle() { return _title; }
+
+
+    public void SetFirstName(string firstName) { _firstName = firstName; }
+    public void SetLastName(string lastName) { _lastName = lastName; }
+    public void SetTitle(string title) { _title = title; }     
 }
