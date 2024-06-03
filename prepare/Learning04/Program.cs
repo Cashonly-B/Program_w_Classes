b2ff68a72fa6f5df6d0646bfc699d935635a92fd
Cashonly-B <bon23011@byui.edu>
2024-06-03 13:28:50 -0600

    DId Leanring 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -3,7 +3,38 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+    { 
     }
+        public class MathAssignment
+        {
+            private string _textbookSection;
+            private string _problems;
+            
+            public string GetHomeworkList()
+            {
+                return _problems;
+            }
+        }
+
+        public class WritingAssignment
+        {
+            private string _title;
+            
+            public string GetHomeworkList()
+            {
+                return _title;
+            }
+        }
+
+        public class Assignment
+        {
+            private string _studentName;
+            private string _topic;
+
+            public string GetSummary()
+            {
+                return _studentName;
+            }
+        }
 }
+
