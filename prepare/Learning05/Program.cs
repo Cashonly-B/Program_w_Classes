cce67c64b04e80ffc045c2be47e1b820b2f3d7d5
Cashonly-B <bon23011@byui.edu>
2024-06-18 10:46:00 -0600

    Did learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        List<RoundShape> shapeList = new List<RoundShape>();
+        shapeList.Add(new Circle(1));
+        shapeList.Add(new Cylinder(1, 2));
+        shapeList.Add(new Sphere(1));
+
+        foreach (RoundShape Shape in shapeList)
+        {
+            Console.WriteLine(Shape.Area());
+        }
     }
 }
